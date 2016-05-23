fd758b61732345756ea07820646afc6ebe6707c6
Flurry2010 <flurry2010@gmail.com>
2016-05-23 08:52:57 +0200

    Flurry : Klassen erstellt

---
+++ src/gui/Infobereich.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by dfleuren on 23.05.2016.
+ */
+public class Infobereich extends JPanel {
+
+    public void zeigeInfo(String info){
+
+    }
+}
