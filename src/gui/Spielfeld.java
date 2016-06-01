b39a581616ee3cc6d82eef589321db2b42f7fb6a
Flurry2010 <flurry2010@gmail.com>
2016-06-01 13:19:44 +0200

    Flurry: Aktiver Spieler Rahmen

--- src/gui/Spielfeld.java
+++ src/gui/Spielfeld.java
@@ -188,7 +188,6 @@ public class Spielfeld extends JPanel {
 
             for (String s : kennungListe) {
                 SpielerPanel tmpPanel = new SpielerPanel(Datenbank.getInstance().selectSpieler(s), haelfte.getRunde(), this);
-                // hier die bUTTOns ändern!!!
                 if(s.equals(gui.getIch())){
                     tmpPanel.getBecher().setEnabled(true);
                     tmpPanel.setBecher(tmpPanel.getBecher());
